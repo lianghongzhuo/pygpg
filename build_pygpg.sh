dc4ca2551c960ed6694082263a1e01d26093912d
Hongzhuo Liang <hongzhuo.liang@agile-robots.com>
2024-12-04 20:16:44 +0100

    remove build folder before build

--- build_pygpg.sh
+++ build_pygpg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-mkdir dependencies && cd dependencies
-git clone https://github.com/lianghongzhuo/gpg.git  # some cmake change to let it work as subproject
-git clone https://github.com/pybind/pybind11
+rm -rf dependencies build *.egg-info *.so && mkdir dependencies && cd dependencies
+git clone https://github.com/lianghongzhuo/gpg.git --depth=1  # some cmake change to let it work as subproject
+git clone https://github.com/pybind/pybind11 --depth=1
 cd ..
 pip install -e .
