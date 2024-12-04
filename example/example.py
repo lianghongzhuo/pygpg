8640229d7d48b0f97d24e66b26e0a9cd7b4be820
Hongzhuo Liang <hongzhuo.liang@agile-robots.com>
2024-12-04 20:25:34 +0100

    remove open3d dependency for example

--- example/example.py
+++ example/example.py
@@ -1,11 +1,6 @@
 import numpy as np
 import pygpg
-import open3d as o3d
-
-# create a open3d box mesh
-box = o3d.geometry.TriangleMesh.create_box(width=0.04, height=0.22, depth=0.06)
-# sample points from the mesh
-points = np.asarray(box.sample_points_uniformly(number_of_points=10000).points)
+points = np.loadtxt("example/box.txt")
 num_samples = 100
 show_grasp = True
 gripper_config_file = "gripper_params.cfg"
